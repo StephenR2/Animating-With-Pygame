b0e114683e00c1e816228951155c97a012e01da5
StephenR2 <stephen@iMac2.local>
2017-12-01 11:44:10 -0500

    Final Version

--- brick.py
+++ brick.py
@@ -3,11 +3,24 @@ class Brick:
 
 
     def __init__(self, surface, height, width, color):
+        """
+        Initializing stating the parameters it needs
+        :param surface: The surface
+        :param height: Height of the brick
+        :param width: Width of the brick
+        :param color: Color of the brick
+        """
         self.surface = surface
         self.height = height
         self.width = width
         self.color = color
         pygame.init()
     def draw(self, x, y):
+        """
+        Method used to actually draw the brick that it has made.
+        :param x: x coord, parameter that draw needs to know in order to draw.
+        :param y: y coord, parameter that draw needs to know in order to draw
+        :return: Returns a drawn brick in the position specified.
+        """
         pygame.draw.rect(self.surface, self.color, (x , y, self.width, self.height), 0)
 
--- diagram.py
+++ diagram.py
@@ -1,11 +1,17 @@
+# Stephen Randall
+# 11/30/17
+# Folder: Unit10Project File: diagram.py
+# This is a simple program that uses pygame to draw a breaker game layout.
 import brick
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 
 pygame.init()
 
-mainSurface = pygame.display.set_mode((700, 400), 0, 32)
+mainSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption("Brick Display")
+# Constants , information that doesn't change has a variable name with all caps.
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -20,31 +26,32 @@ WIDTHSTEPONE = (WHITESPACE * NUMBRICKS - WHITESPACE)
 WIDTH = (SCRWIDTH - WIDTHSTEPONE) / NUMBRICKS
 brickX = 0
 brickY = 380
-for x in range(9):
+for x in range(9):  # Draw bottom layer of bricks
     brickOne = brick.Brick(mainSurface, 20, WIDTH, RED)
     brickOne.draw(brickX, brickY)
-    brickX = brickX + WIDTH + WHITESPACE
-brickY = 360 - WHITESPACE
-brickX = WIDTH + WHITESPACE
-for x in range(7):
+    brickX = brickX + WIDTH + WHITESPACE  # Redefining x coordinate for where the next brick will be drawn.
+brickY = 360 - WHITESPACE  # Redefining the y coord for the next layer of bricks
+brickX = WIDTH + WHITESPACE  # Redefining the x coord for the next layer of bricks
+for a in range(7):
     brickTwo = brick.Brick(mainSurface, 20, WIDTH, PINK)
     brickTwo.draw(brickX, brickY)
     brickX = brickX + WIDTH + WHITESPACE
 brickY = 340 - (2 * WHITESPACE)
+
 brickX = (2 * WIDTH) + (2 * WHITESPACE)
-for x in range(5):
+for c in range(5):
     brickThree = brick.Brick(mainSurface, 20, WIDTH, BLUE)
     brickThree.draw(brickX, brickY)
     brickX = brickX + WIDTH + WHITESPACE
 brickY = 320 - (3 * WHITESPACE)
 brickX = (3 * WIDTH) + (3 * WHITESPACE)
-for x in range(3):
+for b in range(3):
     brickFour = brick.Brick(mainSurface, 20, WIDTH, GREEN)
     brickFour.draw(brickX, brickY)
     brickX = brickX + WIDTH + WHITESPACE
 brickY = 300 - (4 * WHITESPACE)
 brickX = (4 * WIDTH) + (4 * WHITESPACE)
-for x in range(1):
+for h in range(1):
     brickFive = brick.Brick(mainSurface, 20, WIDTH, BLACK)
     brickFive.draw(brickX, brickY)
     brickX = brickX + WIDTH + WHITESPACE
@@ -55,4 +62,3 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-    
